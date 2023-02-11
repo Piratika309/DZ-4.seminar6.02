13812d2b7bf22500268d760d97707f42817b2a54
Natali <piratika309@mail.ru>
2023-02-11 23:51:21 +0300

    Произведение пар чисел

---
+++ example005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти произведение пар чисел в одномерном массиве.Парой считаем первый и последний элемент, второй и предпоследний и тд.
+
+int [] array = new int [10];
+int result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+Console.Write($"{array[i]} \t");
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    result = array[0]*(array[array.Length - 1]);
+}
+   Console.Write($"\n");
+   Console.WriteLine("Прозведение пар чисел первого и последнего массива равно: " + result); 
