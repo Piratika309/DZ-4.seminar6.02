14a401f540eb9486333dd18ba9a41e0ae288753f
Natali <piratika309@mail.ru>
2023-02-11 21:29:00 +0300

    Количество четных и нечетных в массиве

---
+++ example002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив, заполнить случайными положительными трехзначными числами. Показать количество нечетных/четных чисел
+
+int [] array= new int [10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99, 1000);
+}
+
+for (int i = 0; i < array.Length; i++)
+Console.Write ($" {array[i]} \t");
+
+int number = array.Count(i=>i% 2 == 0);
+
+if  (number > 0);
+   {
+        Console.WriteLine("Количество четных цифр в массиве выводится " + number + " раз(а), а количество нечетных цифр равно " + (array.Length-number));
+   }
