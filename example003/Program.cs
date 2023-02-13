9120f2c0057505b538b3391fba5691dee7b489ea
Natali <piratika309@mail.ru>
2023-02-13 19:05:47 +0300

    количество элементов из отрезка

---
+++ example003/Program.cs
@@ -0,0 +1,21 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+
+int [] array= new int [123];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 300);
+}
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+
+if  (array[i] < 100 && array[i]> 9)
+   {
+    count = count+1;
+   }
+
+ Console.WriteLine("Количество  " + count);
+
+
