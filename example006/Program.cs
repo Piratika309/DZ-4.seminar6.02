70de393d72b1077cfc80952761c732508dd12fa3
Natali <piratika309@mail.ru>
2023-02-13 13:58:38 +0300

    разница макс и мин в вещественном массиве

---
+++ example006/Program.cs
@@ -0,0 +1,33 @@
+﻿// В указанном массиве вещественных чисел найдите разницу между максимальным и минимальным  элементом
+
+double [] array = new double [10];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-10,10);
+}
+for (int i = 0; i < array.Length; i++)
+Console.Write ($" {array[i]} \t");
+
+double max = 0;
+
+for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+
+double min = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.Write ("Разница между минимальным и максимальным значением равна: " + (max - (-1)*min));
+
